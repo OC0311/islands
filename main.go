46017ef61275caed0edc7eb72af9cbd861637b53
饶江金 <raojiangjin@gmail.com>
2019-11-02 20:53:33 +0800

    chore:remove int()

--- main.go
+++ main.go
@@ -1,32 +1,10 @@
 package main
 
 import (
-	"log"
-
-	"github.com/boltdb/bolt"
 	"github.com/jiangjincc/islands/block"
 )
 
-const (
-	_blockBucketName = "blocks"
-)
-
-var (
-	db *bolt.DB
-)
-
-func init() {
-	var err error
-
-	db, err = bolt.Open("my.db", 0600, nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-}
-
 func main() {
-	defer db.Close()
 	b := block.CreateBlockchainWithGenesisBlock()
 	_ = b.AddBlockToBlockChain([]byte("trade 100 RMB"))
 	_ = b.AddBlockToBlockChain([]byte("trade 200 RMB"))
